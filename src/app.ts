d2da3374a01a1f11914f4221630aa232f1a67bad
dzavi84 <milanvidanovic@yahoo.com>
2022-02-20 14:35:57 +0100

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
+// autobind decorator
+
+function autobind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
   titleInputElement: HTMLInputElement;
-  descriptionInputElement?: HTMLInputElement;
-  peopleInputElement?: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     // const templateEl=document.getElementById('project-input');
@@ -26,8 +37,25 @@ class ProjectInput {
     this.titleInputElement = this.element.querySelector(
       '#title'
     ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
 
+    this.configure();
     this.attach();
+  }
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    // this.element.addEventListener('submit', this.submitHandler.bind(this));
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
