49e90cae906de5202174aff6db56dc2b731baf37
dzavi84 <milanvidanovic@yahoo.com>
2022-02-21 13:05:01 +0100

    commit

--- dist/app.js
+++ dist/app.js
@@ -45,6 +45,28 @@ function autobind(_, _2, descriptor) {
     };
     return ajdDescriptor;
 }
+// ProjectList Class
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-project-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent =
+            this.type.toUpperCase() + 'PROJECTS';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
 // ProjectInput Class
 class ProjectInput {
     constructor() {
@@ -126,3 +148,5 @@ __decorate([
     __metadata("design:returntype", void 0)
 ], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const activePrjList1 = new ProjectList('finished');
