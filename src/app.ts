1354d102eef680d3b804ae8c2a653831fe12b305
dzavi84 <milanvidanovic@yahoo.com>
2022-02-26 16:25:06 +0100

    commit

--- src/app.ts
+++ src/app.ts
@@ -27,34 +27,62 @@ class ProjectInput {
     // If we not sure that we get some element
     // but we are sure and we can use ! mark
     this.templateElement = document.getElementById(
-      'project-input'
+      'project-input',
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
     const importedNode = document.importNode(
       this.templateElement.content,
-      true
+      true,
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = 'user-input';
     this.titleInputElement = this.element.querySelector(
-      '#title'
+      '#title',
     ) as HTMLInputElement;
     this.descriptionInputElement = this.element.querySelector(
-      '#description'
+      '#description',
     ) as HTMLInputElement;
     this.peopleInputElement = this.element.querySelector(
-      '#people'
+      '#people',
     ) as HTMLInputElement;
 
     this.configure();
 
     this.attach();
   }
+  private clearForm() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
+  private gatherInputMethod(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+    if (
+      enteredDescription.trim().length === 0 ||
+      enteredTitle.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input,please try again!');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
+    const userInput = this.gatherInputMethod();
+    this.clearForm();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+    }
   }
   private configure() {
     this.element.addEventListener('submit', this.submitHandler);
