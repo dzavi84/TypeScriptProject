a572a4d2662cdbcf2ff4e6b23e308b03aac8b949
dzavi84 <milanvidanovic@yahoo.com>
2022-03-10 00:31:34 +0100

    commitlast

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,64 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
-module.exports = {
-    mode: 'development',
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
     entry: './src/app.ts',
     output: {
-        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist',
     },
-    plugins: [new HtmlWebpackPlugin()],
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
 
-    mode: 'production',
-    devtool: 'inline-source-map',
+        new MiniCssExtractPlugin(),
+
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
     module: {
         rules: [{
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }, ],
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, 'css-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
     },
     resolve: {
-        extensions: ['.ts', '.js'],
+        extensions: ['.tsx', '.ts', '.js'],
     },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+
+        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+    } else {
+        config.mode = 'development';
+    }
+    return config;
 };
